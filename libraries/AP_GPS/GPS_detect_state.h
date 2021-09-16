f6dbcf2143854337c4b65ec9ded7582ad66f515a
giacomo892 <giacomo1989@gmail.com>
2021-09-16 16:14:41 +0200

    Remove and disable useless GPS drivers (#7)

--- libraries/AP_GPS/GPS_detect_state.h
+++ libraries/AP_GPS/GPS_detect_state.h
@@ -30,54 +30,8 @@ struct NMEA_detect_state {
     uint8_t ck;
 };
 
-struct SIRF_detect_state {
-    uint16_t checksum;
-    uint8_t step, payload_length, payload_counter;
-};
-
 struct UBLOX_detect_state {
     uint8_t payload_length, payload_counter;
     uint8_t step;
     uint8_t ck_a, ck_b;
-};
-
-struct ERB_detect_state {
-    uint8_t payload_length, payload_counter;
-    uint8_t step;
-    uint8_t ck_a, ck_b;
-};
-
-struct SBP_detect_state {
-    enum {
-        WAITING = 0,
-        GET_TYPE = 1,
-        GET_SENDER = 2,
-        GET_LEN = 3,
-        GET_MSG = 4,
-        GET_CRC = 5
-    } state:8;
-    uint16_t msg_type;
-    uint8_t n_read;
-    uint8_t msg_len;
-    uint16_t crc_so_far;
-    uint16_t crc;
-    uint8_t heartbeat_buff[4];
-};
-
-
-struct SBP2_detect_state {
-    enum {
-        WAITING = 0,
-        GET_TYPE = 1,
-        GET_SENDER = 2,
-        GET_LEN = 3,
-        GET_MSG = 4,
-        GET_CRC = 5
-    } state:8;
-    uint16_t msg_type;
-    uint8_t n_read;
-    uint8_t msg_len;
-    uint16_t crc_so_far;
-    uint16_t crc;
-    uint8_t heartbeat_buff[4];
 };
