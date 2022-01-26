74e6c3de0fc8b7f1ad892117fd87b418e4263886
Michel Pastor <shellixyz@users.noreply.github.com>
2022-01-26 15:48:12 +0100

    Add custom firmware string (#69)

--- libraries/AP_Common/AP_FWVersionDefine.h
+++ libraries/AP_Common/AP_FWVersionDefine.h
@@ -23,6 +23,8 @@
 #include <AP_Common/AP_FWVersion.h>
 #include <AP_Vehicle/AP_Vehicle_Type.h>
 
+#define AP_CUSTOM_FIRMWARE_STRING THISFIRMWARE" Custom v11.2"
+
 /*
   allow vendors to set AP_CUSTOM_FIRMWARE_STRING in hwdef.dat
  */
