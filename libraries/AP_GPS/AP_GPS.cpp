4c10e2f4a42ab45cabe41f4e04ee374b8db6c639
Michel Pastor <shellixyz@gmail.com>
2022-02-01 23:29:22 +0100

    Temporary fix for RTC bug introduced in 9834304525

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -516,13 +516,11 @@ uint64_t AP_GPS::time_epoch_usec(uint8_t instance) const
     if ((istate.last_gps_time_ms == 0 && istate.last_corrected_gps_time_us == 0) || istate.time_week == 0) {
         return 0;
     }
-    uint64_t fix_time_ms;
+    uint64_t fix_time_ms = time_epoch_convert(istate.time_week, istate.time_week_ms);
     // add in the time since the last fix message
     if (istate.last_corrected_gps_time_us != 0) {
-        fix_time_ms = time_epoch_convert(istate.time_week, drivers[instance]->get_last_itow());
         return (fix_time_ms*1000ULL) + (AP_HAL::micros64() - istate.last_corrected_gps_time_us);
     } else {
-        fix_time_ms = time_epoch_convert(istate.time_week, istate.time_week_ms) * 1000ULL;
         return (fix_time_ms + (AP_HAL::millis() - istate.last_gps_time_ms)) * 1000ULL;
     }
 }
