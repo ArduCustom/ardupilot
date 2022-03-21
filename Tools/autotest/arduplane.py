e832b098869209bb12e3f5ddc68c3540d147a63e
Michel Pastor <shellixyz@users.noreply.github.com>
2022-03-21 22:59:55 +0100

    TECS throttle FF changes (#54)

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -2886,6 +2886,7 @@ function'''
             ###################################################################
 
             self.disarm_vehicle(force=True)
+            self.wait_disarmed(timeout=120)
 
         except Exception as e:
             self.print_exception_caught(e)
