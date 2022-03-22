4ef96d200d00886a6552cc7f5c9463f85cd1cb17
Michel Pastor <shellixyz@users.noreply.github.com>
2022-03-22 02:51:14 +0100

    Plane: make changes to angle controllers to be able to leverage PID target filters (#123)

--- libraries/APM_Control/AP_RollController.h
+++ libraries/APM_Control/AP_RollController.h
@@ -24,7 +24,9 @@ public:
     AP_RollController &operator=(const AP_RollController&) = delete;
 
     float get_rate_out(float desired_rate, float scaler);
-    float get_servo_out(int32_t angle_err, int32_t target_angle, float scaler, bool disable_integrator, bool ground_mode);
+    float get_servo_out_using_angle_error(int32_t angle_err, int32_t target_angle, float scaler, bool disable_integrator, bool ground_mode);
+    float get_servo_out_using_angle_target(int32_t target_angle, float scaler, bool disable_integrator, bool ground_mode);
+    float get_servo_out(float desired_rate, float scaler, bool disable_integrator, bool ground_mode);
 
     void reset_I();
 
