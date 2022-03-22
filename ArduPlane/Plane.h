3cc64c6d66037333223ca1890f04c4f9ccb34482
Michel Pastor <shellixyz@gmail.com>
2022-03-22 02:44:17 +0100

    OSD: add elements to display tuned param name and value

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -988,6 +988,7 @@ private:
 # if OSD_ENABLED
     void get_osd_roll_pitch_rad(float &roll, float &pitch) const override;
 #endif
+
     float tecs_hgt_afe(void);
     void efi_update(void);
     void get_scheduler_tasks(const AP_Scheduler::Task *&tasks,
@@ -1208,6 +1209,8 @@ private:
     float rudder_in_expo(bool use_dz) const;
 
 public:
+    AP_Tuning *get_tuning_object(void) override { return &tuning; }
+
     void failsafe_check(void);
 #if AP_SCRIPTING_ENABLED
     bool set_target_location(const Location& target_loc) override;
