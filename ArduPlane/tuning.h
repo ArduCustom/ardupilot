d2bf6c48c1726a9938c6d5dee6a7715431995a39
Michel Pastor <shellixyz@users.noreply.github.com>
2022-03-23 15:56:13 +0100

    Plane: Tuning: more improvements (#127)

--- ArduPlane/tuning.h
+++ ArduPlane/tuning.h
@@ -71,23 +71,25 @@ private:
         TUNING_PIT_D =                        56,
         TUNING_PIT_FF =                       57,
 
-        TUNING_KFF_THRAT2PTCH =               58,
-        TUNING_STAB_PITCH_DOWN =              59,
+        TUNING_TRIM_THROTTLE =                58,
+        TUNING_TRIM_PITCH =                   59,
+        TUNING_KFF_THRAT2PTCH =               60,
+        TUNING_STAB_PITCH_DOWN =              61,
 
-        TUNING_RLL2PTCH =                     60,
-        TUNING_KFF_RDDRMIX =                  61,
+        TUNING_RLL2PTCH =                     62,
+        TUNING_KFF_RDDRMIX =                  63,
 
-        TUNING_TECS_THR_FF_DAMP =             62,
-        TUNING_TECS_THR_FF_FILT =             63,
+        TUNING_TECS_THR_FF_DAMP =             64,
+        TUNING_TECS_THR_FF_FILT =             65,
 
-        TUNING_AGL_ROLL_P =                   64,
-        TUNING_AGL_ROLL_I =                   65,
-        TUNING_AGL_ROLL_D =                   66,
-        TUNING_AGL_ROLL_FLTT =                67,
-        TUNING_AGL_PITCH_P =                  68,
-        TUNING_AGL_PITCH_I =                  69,
-        TUNING_AGL_PITCH_D =                  70,
-        TUNING_AGL_PITCH_FLTT =               71,
+        TUNING_AGL_ROLL_P =                   66,
+        TUNING_AGL_ROLL_I =                   67,
+        TUNING_AGL_ROLL_D =                   68,
+        TUNING_AGL_ROLL_FLTT =                69,
+        TUNING_AGL_PITCH_P =                  70,
+        TUNING_AGL_PITCH_I =                  71,
+        TUNING_AGL_PITCH_D =                  72,
+        TUNING_AGL_PITCH_FLTT =               73,
     };
 
     /*
@@ -101,7 +103,7 @@ private:
         TUNING_SET_ANG_ROLL_PITCH =          5,
         TUNING_SET_VXY =                     6,
         TUNING_SET_AZ =                      7,
-        TUNING_SET_THR2PTCH_MIX =            8,
+        TUNING_SET_TRIM_THR_PTCH =           8,
         TUNING_SET_COORDINATION =            9,
         TUNING_SET_TECS_THR_FF =            10,
         TUNING_SET_AGL_ROLL_PITCH =         11,
@@ -122,7 +124,7 @@ private:
     static const uint8_t tuning_set_ang_roll_pitch[];
     static const uint8_t tuning_set_vxy[];
     static const uint8_t tuning_set_az[];
-    static const uint8_t tuning_set_thr2ptch[];
+    static const uint8_t tuning_set_trim_thr_pitch[];
     static const uint8_t tuning_set_coordination[];
     static const uint8_t tuning_set_tecs_thr_ff[];
     static const uint8_t tuning_set_angle_roll_pitch[];
