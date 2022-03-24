73e9dfdd2bdaadb16f662b7e8d7b5d721a29f633
Michel Pastor <shellixyz@users.noreply.github.com>
2022-03-24 14:33:19 +0100

    Plane: add RC option to enable servo auto trim with RC channel (#136)

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -228,6 +228,7 @@ public:
         FFT_NOTCH_TUNE =     162, // FFT notch tuning function
         MOUNT_LOCK =         163, // Mount yaw lock vs follow
         LOG_PAUSE =          164, // Pauses logging if under logging rate control
+        SERVOS_AUTO_TRIM =   165, // servos auto trim
 
         // inputs from 200 will eventually used to replace RCMAP
         ROLL =               201, // roll input
