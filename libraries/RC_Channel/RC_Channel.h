174279e2880970c66e22736a6878acf02fe39f13
Michel Pastor <shellixyz@gmail.com>
2022-03-29 20:20:36 +0200

    Plane: add throttle curves (#113)

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -617,12 +617,17 @@ protected:
         SUPPRESS_CRSF_MESSAGE   = (1U << 9), // suppress CRSF mode/rate message for ELRS systems
         MULTI_RECEIVER_SUPPORT  = (1U << 10), // allow multiple receivers
         USE_CRSF_LQ_AS_RSSI     = (1U << 11), // returns CRSF link quality as RSSI value, instead of RSSI
+        PLANE_DISABLE_MAN_THR_EXPO  = (1U << 23), // plane only: disable throttle expo in manual mode
     };
 
     void new_override_received() {
         has_new_overrides = true;
         _has_had_override = true;
     }
+
+protected:
+
+    AP_Int32  _options;
 
 private:
     static RC_Channels *_singleton;
@@ -635,7 +640,6 @@ private:
     bool _has_had_override; // true if we have had an override on any channel
 
     AP_Float _override_timeout;
-    AP_Int32  _options;
     AP_Int32  _protocols;
 
     RC_Channel *flight_mode_channel() const;
