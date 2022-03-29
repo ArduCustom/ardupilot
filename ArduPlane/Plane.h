e74922cb523a5eed2c0f1012f60e9b9e614db955
Michel Pastor <shellixyz@gmail.com>
2022-03-29 20:20:36 +0200

    Plane: add throttle curves (#113)

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1078,6 +1078,7 @@ private:
     void set_servos();
     void set_servos_manual_passthrough(void);
     void apply_throttle_dz(void);
+    void apply_throttle_expo(void);
     void set_servos_controlled(void);
     void set_servos_old_elevons(void);
     void set_servos_flaps(void);
