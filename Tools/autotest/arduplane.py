c90358d630a4ea2e4c7b6d1c6b5e461a859ae2fa
Michel Pastor <shellixyz@gmail.com>
2022-04-01 02:14:58 +0200

    Plane: add option to allow gliding in auto throttle modes (#149)

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -2359,6 +2359,24 @@ class AutoTestPlane(AutoTest):
         self.set_rc(2, 1500)
         self.fly_home_land_and_disarm()
 
+    def test_cruise_gliding(self):
+        self.set_parameter("FLIGHT_OPTIONS", 1 << 22)
+        self.takeoff(alt=200)
+        self.change_mode('CRUISE')
+        self.progress("Climbing to 250m at half speed")
+        self.set_rc(3, 1500)
+        self.set_rc(2, 1100)
+        self.wait_altitude(250, 260, timeout=100, relative=True)
+        self.progress("Gliding back down to 200m")
+        self.set_rc(3, 1000)
+        self.wait_servo_channel_value(3, 1000, timeout=4, comparator=operator.eq)
+        self.wait_altitude(195, 205, timeout=100, relative=True)
+        self.progress("Climbing back up to 250m at full speed")
+        self.set_rc(3, 2000)
+        self.wait_servo_channel_value(3, 1200, timeout=4, comparator=operator.ge)
+        self.wait_altitude(250, 260, timeout=100, relative=True)
+        self.disarm_vehicle(force=True)
+
     def CPUFailsafe(self):
         '''In lockup Plane should copy RC inputs to RC outputs'''
         self.plane_CPUFailsafe()
