1cc636eb1b872e3cd3baa417ca1d76679469d75d
Michel Pastor <shellixyz@users.noreply.github.com>
2022-04-08 15:28:34 +0200

    Plane: fix throttle output unsynchronized concurrent access (#167)

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -207,6 +207,9 @@ private:
     AP_YawController yawController{aparm};
     AP_SteerController steerController{};
 
+    HAL_Semaphore _thr_sem;
+    float _throttle_output;
+
     // Training mode
     bool training_manual_roll;  // user has manual roll control
     bool training_manual_pitch; // user has manual pitch control
