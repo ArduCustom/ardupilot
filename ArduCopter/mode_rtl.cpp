26218174b6e07b5867e1450a1a755352de38dc3d
Michel Pastor <shellixyz@gmail.com>
2022-04-11 01:57:55 +0200

    Plane+Copter: convert/uniformize RTL altitude units to meter (#157)

--- ArduCopter/mode_rtl.cpp
+++ ArduCopter/mode_rtl.cpp
@@ -434,7 +434,7 @@ void ModeRTL::build_path()
     rtl_path.climb_target = Location(rtl_path.origin_point.lat, rtl_path.origin_point.lng, rtl_path.return_target.alt, rtl_path.return_target.get_alt_frame());
 
     // descent target is below return target at rtl_alt_final
-    rtl_path.descent_target = Location(rtl_path.return_target.lat, rtl_path.return_target.lng, g.rtl_alt_final, Location::AltFrame::ABOVE_HOME);
+    rtl_path.descent_target = Location(rtl_path.return_target.lat, rtl_path.return_target.lng, g.rtl_alt_final * 100, Location::AltFrame::ABOVE_HOME);
 
     // set land flag
     rtl_path.land = g.rtl_alt_final <= 0;
@@ -477,7 +477,7 @@ void ModeRTL::compute_return_target()
     if (alt_type == ReturnTargetAltType::RANGEFINDER) {
         if (copter.get_rangefinder_height_interpolated_cm(curr_alt)) {
             // set return_target.alt
-            rtl_path.return_target.set_alt_cm(MAX(curr_alt + MAX(0, g.rtl_climb_min), MAX(g.rtl_altitude, RTL_ALT_MIN)), Location::AltFrame::ABOVE_TERRAIN);
+            rtl_path.return_target.set_alt_cm(MAX(curr_alt + MAX(0, g.rtl_climb_min_m * 100), MAX(g.rtl_altitude_m * 100, RTL_ALT_MIN)), Location::AltFrame::ABOVE_TERRAIN);
         } else {
             // fallback to relative alt and warn user
             alt_type = ReturnTargetAltType::RELATIVE;
@@ -518,8 +518,8 @@ void ModeRTL::compute_return_target()
     int32_t target_alt = MAX(rtl_path.return_target.alt, 0);
 
     // increase target to maximum of current altitude + climb_min and rtl altitude
-    target_alt = MAX(target_alt, curr_alt + MAX(0, g.rtl_climb_min));
-    target_alt = MAX(target_alt, MAX(g.rtl_altitude, RTL_ALT_MIN));
+    target_alt = MAX(target_alt, curr_alt + MAX(0, g.rtl_climb_min_m * 100));
+    target_alt = MAX(target_alt, MAX(g.rtl_altitude_m * 100, RTL_ALT_MIN));
 
     // reduce climb if close to return target
     float rtl_return_dist_cm = rtl_path.return_target.get_distance(rtl_path.origin_point) * 100.0f;
