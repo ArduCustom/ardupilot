c6c5cceecc45196d65fac887c2b452ff306533d8
Michel Pastor <shellixyz@users.noreply.github.com>
2022-04-14 17:53:26 +0200

    Plane: restart mission when switching to auto if not flying and mission starts with takeoff (#183)

--- libraries/AP_Mission/AP_Mission.h
+++ libraries/AP_Mission/AP_Mission.h
@@ -431,7 +431,7 @@ public:
     void start_or_resume();
 
     /// check mission starts with a takeoff command
-    bool starts_with_takeoff_cmd();
+    bool starts_with_takeoff_cmd(bool begining = false);
 
     /// reset - reset mission to the first command
     void reset();
