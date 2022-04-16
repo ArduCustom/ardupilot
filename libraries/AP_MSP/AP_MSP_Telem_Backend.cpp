4e28f5d557c93c92e71b9fe294dd1ec352282d75
Michel Pastor <shellixyz@gmail.com>
2022-04-16 23:35:40 +0200

    Add MSP option to send LQ instead of RSSI

--- libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
+++ libraries/AP_MSP/AP_MSP_Telem_Backend.cpp
@@ -29,6 +29,7 @@
 #include <AP_RTC/AP_RTC.h>
 #include <GCS_MAVLink/GCS.h>
 #include <AP_Vehicle/AP_Vehicle.h>
+#include <AP_RCProtocol/AP_RCProtocol_CRSF.h>
 
 #include "AP_MSP.h"
 #include "AP_MSP_Telem_Backend.h"
@@ -1251,7 +1252,11 @@ bool AP_MSP_Telem_Backend::get_rssi(float &rssi) const
     if (!ap_rssi->enabled()) {
         return false;
     }
-    rssi =  ap_rssi->read_receiver_rssi(); // range is [0-1]
+    if (AP::msp()->is_option_enabled(AP_MSP::Option::LQ_INSTEAD_OF_RSSI)) {
+        rssi = AP::crsf()->get_rx_link_quality() * 0.01f;
+    } else {
+        rssi =  ap_rssi->read_receiver_rssi(); // range is [0-1]
+    }
     return true;
 }
 #endif //HAL_MSP_ENABLED
