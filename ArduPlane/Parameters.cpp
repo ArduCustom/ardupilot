e15fd6dbc3ca8193362c313a1275bee7d1c4a14c
Michel Pastor <shellixyz@users.noreply.github.com>
2022-04-16 17:07:48 +0200

    Plane: add leveling altitude option for emergency landing on FS (#190)

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -574,6 +574,15 @@ const AP_Param::Info Plane::var_info[] = {
     // @User: Standard
     GSCALAR(fs_emergency_landing_delay, "FS_ELAND_DELAY", -1),
 
+    // @Param: FS_ELAND_LVLALT
+    // @DisplayName: Failsafe emergency landing leveling altitude
+    // @Description: Altitude below which the plane will level in FS RTL emergency landing. Set to -1 to disable and continue landing in spiral until touching the ground.
+    // @Units: m
+    // @Range: 0 600
+    // @Increment: 0.1
+    // @User: Standard
+    GSCALAR(fs_emergency_landing_leveling_altitude, "FS_ELAND_LVLALT", -1),
+
     // @Param: FS_GCS_ENABL
     // @DisplayName: GCS failsafe enable
     // @Description: Enable ground control station telemetry failsafe. Failsafe will trigger after FS_LONG_TIMEOUT seconds of no MAVLink heartbeat messages. There are three possible enabled settings. Setting FS_GCS_ENABL to 1 means that GCS failsafe will be triggered when the aircraft has not received a MAVLink HEARTBEAT message. Setting FS_GCS_ENABL to 2 means that GCS failsafe will be triggered on either a loss of HEARTBEAT messages, or a RADIO_STATUS message from a MAVLink enabled 3DR radio indicating that the ground station is not receiving status updates from the aircraft, which is indicated by the RADIO_STATUS.remrssi field being zero (this may happen if you have a one way link due to asymmetric noise on the ground station and aircraft radios).Setting FS_GCS_ENABL to 3 means that GCS failsafe will be triggered by Heartbeat(like option one), but only in AUTO mode. WARNING: Enabling this option opens up the possibility of your plane going into failsafe mode and running the motor on the ground it it loses contact with your ground station. If this option is enabled on an electric plane then you should enable ARMING_REQUIRED.
