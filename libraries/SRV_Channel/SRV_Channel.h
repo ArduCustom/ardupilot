60487715039832e00a87830a523266037215bce3
Michel Pastor <shellixyz@gmail.com>
2022-04-17 20:02:48 +0200

    Remove Volz and Robotis servo drivers

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -17,8 +17,6 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_Param/AP_Param.h>
 #include <AP_Common/Bitmask.h>
-#include <AP_Volz_Protocol/AP_Volz_Protocol.h>
-#include <AP_RobotisServo/AP_RobotisServo.h>
 #include <AP_SBusOut/AP_SBusOut.h>
 #include <AP_BLHeli/AP_BLHeli.h>
 #include <AP_FETtecOneWire/AP_FETtecOneWire.h>
@@ -620,23 +618,11 @@ private:
     static SRV_Channel *channels;
     static SRV_Channels *_singleton;
 
-#if AP_VOLZ_ENABLED
-    // support for Volz protocol
-    AP_Volz_Protocol volz;
-    static AP_Volz_Protocol *volz_ptr;
-#endif
-
 #ifndef HAL_BUILD_AP_PERIPH
     // support for SBUS protocol
     AP_SBusOut sbus;
     static AP_SBusOut *sbus_ptr;
 #endif // HAL_BUILD_AP_PERIPH
-
-#if AP_ROBOTISSERVO_ENABLED
-    // support for Robotis servo protocol
-    AP_RobotisServo robotis;
-    static AP_RobotisServo *robotis_ptr;
-#endif
 
 #if HAL_SUPPORT_RCOUT_SERIAL
     // support for BLHeli protocol
