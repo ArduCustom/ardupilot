66155c6811219b76b7717528b5d1bd91b3ed9458
Michel Pastor <shellixyz@users.noreply.github.com>
2022-04-23 17:02:14 +0200

    OSD: fix RSSI varying length (#196)

--- libraries/AP_OSD/AP_OSD_Screen.cpp
+++ libraries/AP_OSD/AP_OSD_Screen.cpp
@@ -1904,7 +1904,7 @@ void AP_OSD_Screen::draw_rssi(uint8_t x, uint8_t y)
 {
     AP_RSSI *ap_rssi = AP_RSSI::get_singleton();
     if (ap_rssi) {
-        const uint8_t rssiv = ap_rssi->read_receiver_rssi() * 100;
+        const uint8_t rssiv = MIN(99, lrintf(ap_rssi->read_receiver_rssi() * 100));
         backend->write(x, y, rssiv < osd->warn_rssi, "%c%2d", SYMBOL(SYM_RSSI), rssiv);
     }
 }
