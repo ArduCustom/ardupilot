323fcc78db4b4c6f6dfc4b2d4f9b5dfd40a4faf2
Michel Pastor <shellixyz@gmail.com>
2022-05-01 00:20:13 +0200

    Plane: remove stick mixing (#179)

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -1238,13 +1238,6 @@ float QuadPlane::get_pilot_input_yaw_rate_cds(void) const
         return 0;
     }
 
-    if ((plane.g.stick_mixing == StickMixing::NONE) &&
-        (plane.control_mode == &plane.mode_qrtl ||
-         plane.control_mode->is_guided_mode() ||
-         in_vtol_auto())) {
-        return 0;
-    }
-
     // add in rudder input
     const float yaw_rate_max = command_model_pilot.get_rate();
     float max_rate = yaw_rate_max;
