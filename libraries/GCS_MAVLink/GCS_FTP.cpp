5d3585d8eedc81b0e1bddd2573ee8545598f5c78
Michel Pastor <shellixyz@gmail.com>
2022-05-20 00:28:30 +0200

    GCS_MAVLink/GCS_FTP: fix warning with GCC 11.1

--- libraries/GCS_MAVLink/GCS_FTP.cpp
+++ libraries/GCS_MAVLink/GCS_FTP.cpp
@@ -579,7 +579,7 @@ int GCS_MAVLINK::gen_dir_entry(char *dest, size_t space, const char *path, const
     }
 
     if (is_file) {
-        const size_t full_path_len = strlen(path) + strnlen(entry->d_name, 256); // FIXME: Really should do better then just hardcoding 256
+        const size_t full_path_len = strlen(path) + strnlen(entry->d_name, 255); // FIXME: Really should do better then just hardcoding 256
         char full_path[full_path_len + 2];
         hal.util->snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
         struct stat st;
