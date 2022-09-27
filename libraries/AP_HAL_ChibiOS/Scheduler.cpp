644d7b07f954958b385973c3883364eea7ff24c3
Michel Pastor <shellixyz@gmail.com>
2022-09-27 22:19:55 +0200

    AP_HAL_ChibiOS: fix compilation with GCC 12.2.1

--- libraries/AP_HAL_ChibiOS/Scheduler.cpp
+++ libraries/AP_HAL_ChibiOS/Scheduler.cpp
@@ -565,7 +565,10 @@ void Scheduler::check_low_memory_is_zero()
     // we can't do address 0, but can check next 3 bytes
     const uint8_t *addr0 = (const uint8_t *)0;
     for (uint8_t i=1; i<4; i++) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
         if (addr0[i] != 0) {
+#pragma GCC diagnostic pop
             AP_memory_guard_error(1023);
             break;
         }
