3050faf3bc022fd30733ff7eb47fccf092dea528
Michel Pastor <shellixyz@gmail.com>
2023-03-07 17:46:25 +0000

    Switch to EKF2 only on boards <= 1MB flash

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -24,13 +24,13 @@
 #include <AP_HAL/AP_HAL_Boards.h>
 #include <AP_HAL/Semaphores.h>
 
-#ifndef HAL_NAVEKF2_AVAILABLE
+#ifndef HAL_NAVEKF3_AVAILABLE
 // only default to EK2 enabled on boards with over 1M flash
-#define HAL_NAVEKF2_AVAILABLE (BOARD_FLASH_SIZE>1024)
+#define HAL_NAVEKF3_AVAILABLE (BOARD_FLASH_SIZE>1024)
 #endif
 
-#ifndef HAL_NAVEKF3_AVAILABLE
-#define HAL_NAVEKF3_AVAILABLE 1
+#ifndef HAL_NAVEKF2_AVAILABLE
+#define HAL_NAVEKF2_AVAILABLE 1
 #endif
 
 #ifndef AP_AHRS_SIM_ENABLED
